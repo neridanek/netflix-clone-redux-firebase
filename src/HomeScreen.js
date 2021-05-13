ab533184dce5146c8d2ec29769f224de6615673a
neridanek <wiktorn.op@gmail.com>
2021-05-13 16:03:52 +0200

    ...

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -3,12 +3,12 @@ import Nav from './components/Nav'
 import Banner from './components/Banner'
 import Row from './components/Row'
 import requests from './Requests'
-import OverflowScrolling from 'react-overflow-scrolling';
+import styles from './HomeScreen.module.scss'
 
 
 const HomeScreen = () => {
     return (
-        <div>
+        <div className={styles.homeScreen}>
             <Nav />
             <Banner />
         
@@ -18,12 +18,12 @@ const HomeScreen = () => {
         fetchUrl={requests.fetchNetflixOriginals}
         />
         <Row title="Trending now" fetchUrl={requests.fetchTrending} />
-        <Row title="Top Rated" fetchUrl={requests.    fetchTrending} />
-        <Row title="Action Movies" fetchUrl={requests.    fetchActionMovies} />
-        <Row title="Comedy Movies" fetchUrl={requests.    fetchComedyMovies} />
-        <Row title="Horror Movies" fetchUrl={requests.    fetchHorrorMovies} />
-        <Row title="Romance Movies" fetchUrl={requests.   fetchRomanceMovies} />
-        <Row title="Documentaries" fetchUrl={requests.    fetchDocumentaries} />
+        <Row title="Top Rated" fetchUrl={requests.fetchTrending} />
+        <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+        <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+        <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+        <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+        <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
         
         </div>
     )
